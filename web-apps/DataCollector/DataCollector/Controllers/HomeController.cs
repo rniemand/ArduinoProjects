7fe81a28967cde6048f51c34deb3def797a78e95
Richard Niemand <niemand.richard@gmail.com>
2017-02-27 22:55:43 +0200

    Adding navigation pages

--- web-apps/DataCollector/DataCollector/Controllers/HomeController.cs
+++ web-apps/DataCollector/DataCollector/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace DataCollector.Controllers
 {
@@ -11,6 +7,20 @@ namespace DataCollector.Controllers
     public ActionResult Index()
     {
       ViewBag.Title = "Home Page";
+
+      return View();
+    }
+
+    public ActionResult Temperature()
+    {
+      ViewBag.Title = "Temperature";
+
+      return View();
+    }
+
+    public ActionResult Light()
+    {
+      ViewBag.Title = "Light";
 
       return View();
     }
